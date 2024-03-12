00f165098ba410905a11670361d84398332ff530
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-12 16:19:23 +0200

    String slicing continues

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -3,21 +3,7 @@
 parrot = "Norwegian Blue"
 
 print(parrot[0:6]) # Norweg
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[-14:-8])
 
-print(parrot[10:14])
-print(parrot[10:])
-
-print(parrot[:6])
-print(parrot[6:])
-
-print(parrot[:6] + parrot[6:])
-
-print(parrot[:])
-
-#                    1         2
-#          01234567890123456789012345
-letters = "abcdefghijklmnopqrstuvwxyz"
-print(letters[13:16] + letters[4])
+print(parrot[-4:-2])
+print(parrot[-4:12])
