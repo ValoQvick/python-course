f3545337824af5260b678882215f61f6d1555a56
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-12 16:37:00 +0200

    Challenge bit
    Challenge made by instructor

---
+++ HelloWorld/sliceback.py
@@ -0,0 +1,9 @@
+#          01234567890123456789012345
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+
+print(letters[16:13:-1])    # qpo
+print(letters[4::-1])       # edcba
+print(letters[:17:-1])      # last 8 characters in reverse order
