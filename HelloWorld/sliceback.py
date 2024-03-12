195a8a537bceadf3bc6b946c4e6d6a301d1208c2
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-12 17:14:36 +0200

    Sequencing

--- HelloWorld/sliceback.py
+++ HelloWorld/sliceback.py
@@ -7,3 +7,8 @@ print(backwards)
 print(letters[16:13:-1])    # qpo
 print(letters[4::-1])       # edcba
 print(letters[:17:-1])      # last 8 characters in reverse order
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])   # might cause errors when empty
