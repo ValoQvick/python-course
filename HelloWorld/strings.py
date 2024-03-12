2816c40ae840b93b16be2be18ac06a35ca1b1cbd
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-12 13:19:52 +0200

    Strings & escape characters
    Strings & escape characters

---
+++ HelloWorld/strings.py
@@ -0,0 +1,12 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+print(greeting + name)
+
+#if we want space, we can add that too
+print(greeting + ' ' + name)
