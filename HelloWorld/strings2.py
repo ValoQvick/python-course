47ff9c021cee9a4a94a78a1575170c8869686759
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-12 16:15:46 +0200

    Slicing strings

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -1,31 +1,23 @@
 #                   1
 #         01234567890123
-# -       43210987654321
 parrot = "Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6]) # Norweg
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
 
-print(parrot[3])    # w
-print(parrot[4])    # e
-print(parrot[9])             #
-print(parrot[3])    # w
-print(parrot[6])    # i
-print(parrot[8])    # n
+print(parrot[10:14])
+print(parrot[10:])
 
-print()
+print(parrot[:6])
+print(parrot[6:])
 
-print(parrot[-11])    # w
-print(parrot[-10])    # e
-print(parrot[-5])     #
-print(parrot[-11])    # w
-print(parrot[-8])     # i
-print(parrot[-6])     # n
+print(parrot[:6] + parrot[6:])
 
-print()
+print(parrot[:])
 
-print(parrot[3 - 14])    # w
-print(parrot[4 - 14])    # e
-print(parrot[9 - 14])    #
-print(parrot[3 - 14])    # w
-print(parrot[6 - 14])    # i
-print(parrot[8 - 14])    # n
+#                    1         2
+#          01234567890123456789012345
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters[13:16] + letters[4])
