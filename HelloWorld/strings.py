65cde9d2c1ae35aa8e48453f55d8e6013125f640
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-13 09:37:14 +0200

    f-string and string interpolation
    Interpolation mostly just in python 2

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -5,7 +5,7 @@ print('We can even include "quotes" in strings')
 print("Hello" + " world")
 
 greeting = "Hello"
-name = input("Please enter your name: ")
+name = "Valo" #input("Please enter your name: ")
 print(greeting + name)
 
 #if we want space, we can add that too
@@ -18,5 +18,9 @@ print(type(greeting))
 print(type(age))
 
 age_in_words = "2 years"
-# print(name + " is " + age + " years old.")
+print(name + f" is {age} years old.")
 print(type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
