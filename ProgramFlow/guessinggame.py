753bafe21a8f11440aa8cebf0a7dedc112e59c29
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-13 12:55:28 +0200

    Guessing game
    Enhancing the code

--- ProgramFlow/guessinggame.py
+++ ProgramFlow/guessinggame.py
@@ -3,9 +3,32 @@ answer = 5
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher.")
-elif guess > answer:
-    print("Please guess lower.")
+if guess == answer:
+    print("Hey, you got it! :)")
 else:
-    print("Correct! :)")
+    if guess < answer:
+        print("Please guess higher: ")
+    else:   # guess must be greater than answer
+        print("Please guess lower: ")
+    guess = int(input())
+    if guess == answer:
+        print("Well done! :)")
+    else:
+        print("Sorry, not correct. :(")
+
+# if guess < answer:
+#     print("Please guess higher: ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry :(")
+# elif guess > answer:
+#     print("Please guess lower: ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry :(")
+# else:
+#     print("Correct! :)")
