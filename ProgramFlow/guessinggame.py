e9c03a21c4af648afb09409b8b2329ef3caa881e
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-15 12:01:29 +0200

    Finishing touches to guessing game

--- ProgramFlow/guessinggame.py
+++ ProgramFlow/guessinggame.py
@@ -4,20 +4,22 @@ highest = 10
 answer = random.randint(1, highest)
 print(answer)   #TODO: Remove after testing
 
+guess = 0   # initialise to any number that doesn't equal the answer
 print("Please guess a number between 1 and {}: ".format(highest))
-guess = int(input())
 
 while guess != answer:
+    guess = int(input())
+
     if guess == 0:
         print("You quit the game.")
+        break
+    if guess == answer:
+        print("Hey, you got it! :)")
         break
     elif guess < answer:
         print("Please guess higher: ")
     elif guess > answer:   # guess must be greater than answer
         print("Please guess lower: ")
-    guess = int(input())
-
-print("Hey, you got it! :)")
 
 # if guess == answer:
 #     print("Hey, you got it! :)")
