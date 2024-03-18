1f774a2bf22192bbe776ff8c2ddc3f8ad2aab242
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-18 11:12:14 +0200

    Optional extra challenge added

--- ProgramFlow/sectionchallenge.py
+++ ProgramFlow/sectionchallenge.py
@@ -1,15 +1,16 @@
-print("Please choose your option from the list below: ")
-print("1:\tLearn Python")
-print("2:\tPlay Stardew Valley")
-print("3:\tGo hiking")
-print("4:\tEat something")
-print("5:\tTime to sleep")
-print("0:\tExit")
-
+choice = "-"
 while True:
-    choice = input()
-
     if choice == "0":
         break
     elif choice in "12345":
         print("You chose {}".format(choice))
+    else:
+        print("Please choose your option from the list below: ")
+        print("1:\tLearn Python")
+        print("2:\tPlay Stardew Valley")
+        print("3:\tGo hiking")
+        print("4:\tEat something")
+        print("5:\tTime to sleep")
+        print("0:\tExit")
+
+    choice = input()
