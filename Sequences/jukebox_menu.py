5ecf174166c84bfa5fcc7c4ddcde5f3580a12155
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-26 14:10:32 +0200

    Simple Jukebox - Finishing the code
    No error handling

--- Sequences/jukebox_menu.py
+++ Sequences/jukebox_menu.py
@@ -1,6 +1,7 @@
 from nested_data import albums
 
 SONGS_LIST_INDEX = 3  # constants in all capitals
+SONG_TITLE_INDEX = 1
 
 while True:
     print("Please choose your album (invalid choice exits):")
@@ -9,10 +10,18 @@ while True:
 
     choice = int(input())
     if 1 <= choice <= len(albums):
-        songs_list = albums[choice -1][SONGS_LIST_INDEX]
+        songs_list = albums[choice - 1][SONGS_LIST_INDEX]
     else:
         break
 
-    print(albums[choice -1])
-    print(songs_list)
-    print()
+    print("Please choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+    print("Now playing {}".format(title))
+    print("=" * 40)
