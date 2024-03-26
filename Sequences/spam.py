180ccbe88f90777e2b8646f8340f42a2cdfe3c32
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-26 10:07:39 +0200

    Nested lists and code style

---
+++ Sequences/spam.py
@@ -0,0 +1,10 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "sausage", "bacon", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon"],
+]
