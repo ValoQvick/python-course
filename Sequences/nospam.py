e18807167ae7be50207164f12e42e663e621c6c2
Valo Qvick <jenni.heikkila89@gmail.com>
2024-03-26 10:43:02 +0200

    join method

--- Sequences/nospam.py
+++ Sequences/nospam.py
@@ -10,15 +10,8 @@ menu = [
 ]
 
 for meal in menu:
-    for item in meal:
-        if item != "spam":
-            print(item, end=", ")
+    for index in range(len(meal) - 1, - 1, - 1):
+        if meal[index] == "spam":
+            del(meal[index])
 
-    print()
-
-# for meal in menu:
-#     for index in range(len(meal) - 1, - 1, - 1):
-#         if meal[index] == "spam":
-#             del(meal[index])
-#
-#     print(meal)
+    print(", ".join(meal))
