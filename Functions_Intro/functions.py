d8f222865a3cdf70555d137eab457e005628260d
Valo Qvick <jenni.heikkila89@gmail.com>
2024-04-10 09:39:06 +0300

    Parameters and arguments

--- Functions_Intro/functions.py
+++ Functions_Intro/functions.py
@@ -5,3 +5,12 @@ def multiply(a, b):
 
 answer = multiply(10.5, 4)
 print("Answer is: ", answer)
+
+forty_two = multiply(6, 7)
+print("Answer is: ", forty_two)
+
+print()
+
+for val in range(1, 5):
+    two_times = multiply(2, val)
+    print(two_times)
