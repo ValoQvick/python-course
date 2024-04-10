3fef6204d28f4c8ce2fd86576aa1607b6c5887f3
Valo Qvick <jenni.heikkila89@gmail.com>
2024-04-10 11:42:10 +0300

    width challenge
    Modifying the banner_text function so that it takes the width as another argument

--- Functions_Intro/banner.py
+++ Functions_Intro/banner.py
@@ -1,5 +1,5 @@
-def banner_text(text):
-    screen_width = 50
+def banner_text(screen_width, text):
+    # screen_width = 50
     if len(text) > screen_width - 4:
         raise ValueError("String {0} is larger than specified width {1}"
                          .format(text, screen_width))
@@ -11,14 +11,14 @@ def banner_text(text):
         print(output_string)
 
 
-banner_text("*")
-banner_text("Always look on the bright side of life...")
-banner_text("If life seems jolly rotten,")
-banner_text("There's something you've forgotten!")
-banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
-banner_text("When you're feeling in the dumps,")
-banner_text("Don't be silly chumps,")
-banner_text("Just purse your lips and whistle - that's the thing!")
-banner_text("And... always look on the bright side of life...")
-banner_text("*")
+banner_text(80, "*")
+banner_text(80, "Always look on the bright side of life...")
+banner_text(80, "If life seems jolly rotten,")
+banner_text(80, "There's something you've forgotten!")
+banner_text(80, "And that's to laugh and smile and dance and sing,")
+banner_text(80, " ")
+banner_text(80, "When you're feeling in the dumps,")
+banner_text(80, "Don't be silly chumps,")
+banner_text(80, "Just purse your lips and whistle - that's the thing!")
+banner_text(80, "And... always look on the bright side of life...")
+banner_text(80, "*")
